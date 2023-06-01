0f64575574c226e0e778d8167aedc2bf29c8f514
watmot <tomwatson840@gmail.com>
2023-06-01 10:12:04 +0100

    Tidy up testrail api tests

--- lib/api/testrail.test.ts
+++ lib/api/testrail.test.ts
@@ -1,57 +1,112 @@
-import {_request, get, queryParser} from './testrail'
-
-import axios from 'axios'
+import { _queryParser, _request, get, post } from './testrail';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 // Mocks
 jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+// Test Data
+const axiosResolved = {
+  status: 200,
+  data: [
+    {
+      announcement: 'test',
+      completed_on: 12345,
+      default_role: 'test',
+      default_role_id: 12345,
+      groups: [],
+      id: 1,
+      is_completed: 0,
+      name: 'Test Project',
+      show_announcement: 1,
+      suite_mode: 1,
+      url: 'https://test.testail.com'
+    }
+  ]
+};
 
 describe('TestRail API', () => {
   afterEach(() => {
     jest.resetAllMocks();
-  })
+  });
 
-  describe('Query Parser', () => {
+  describe('._queryParser', () => {
     it('should return a query string', () => {
-      const params = {one: 'one', 'two': false, 'three': 1}
+      const params = { one: 'one', two: false, three: 1 };
 
-      const query = queryParser(params);
+      const query = _queryParser(params);
 
       expect(query).toBe('&one=one&two=false&three=1');
-    })
+    });
 
     it('should return an empty string if the params object is empty', () => {
-      const params = {}
+      const params = {};
 
-      const query = queryParser(params);
+      const query = _queryParser(params);
 
       expect(query).toBe('');
-    })
-  })
-  
-  describe('Request wrapper', () => {
-    it('should call the request function once', async () => {
-      const uri = 'test';
+    });
+  });
 
-      await _request('get', uri);
-      const callArgs = jest.mocked(axios).mock.lastCall[0];
-  
-      expect(axios).toHaveBeenCalledTimes(1);
-      expect(callArgs).toHaveProperty('method', 'get'); 
-      expect(callArgs.url).toContain(uri);
-    })
-  })
+  describe('._request', () => {
+    let result: AxiosResponse;
+    let callArgs: AxiosRequestConfig;
+    const uri = 'test';
 
-  describe('GET request', () => {
-    it('should call the request function once', async () => {
-      const uri = 'get_projects';
+    beforeEach(async () => {
+      // Mock the resolved value from the Axios call
+      mockedAxios.get.mockResolvedValueOnce(axiosResolved);
+      result = await _request('get', uri);
+      const { lastCall } = jest.mocked(mockedAxios.get).mock;
+      callArgs = (lastCall && lastCall[0]) as AxiosRequestConfig;
+    });
 
-      await get(uri);
-      const callArgs = jest.mocked(axios).mock.lastCall[0];
-  
-      expect(axios).toHaveBeenCalledTimes(1);
-      expect(callArgs).toHaveProperty('method', 'get'); 
-      expect(callArgs.url).toContain(uri);
-    })
-  })
-})
+    it('should call axios once', async () => {
+      expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    });
 
+    it('should call axios with a url which contains the uri', () => {
+      expect(callArgs).toContain(uri);
+    });
+
+    it('should return the entire response from axios', () => {
+      expect(result).toEqual(axiosResolved);
+    });
+  });
+
+  describe('.get', () => {
+    let result: AxiosResponse;
+    const uri = 'test';
+
+    beforeEach(async () => {
+      mockedAxios.get.mockResolvedValueOnce(axiosResolved);
+      result = await get(uri);
+    });
+
+    it('should call the axios.get once', async () => {
+      expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return the response', () => {
+      expect(result).toEqual(axiosResolved);
+    });
+  });
+
+  describe('.post', () => {
+    let result: AxiosResponse;
+    const uri = 'test';
+
+    beforeEach(async () => {
+      mockedAxios.post.mockResolvedValueOnce(axiosResolved);
+      result = await post(uri);
+    });
+
+    it('should call the axios.post once', async () => {
+      expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return the response', async () => {
+      expect(result).toEqual(axiosResolved);
+    });
+  });
+});
--- lib/api/testrail.ts
+++ lib/api/testrail.ts
@@ -1,39 +1,54 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 const HOST = process.env.TESTRAIL_HOST,
-  USERNAME = process.env.TESTRAIL_USER, 
-  API_KEY= process.env.TESTRAIL_API_KEY;
+  USERNAME = process.env.TESTRAIL_USER,
+  API_KEY = process.env.TESTRAIL_API_KEY;
 
-export const queryParser = (params: Api.Params) => {
+class TestRailError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'TestRailError';
+  }
+}
+
+export const _queryParser = (params: Api.Params) => {
   let query = '';
 
   // Loop through param keys to generate query string
   Object.keys(params).forEach((item, index) => {
-    query += `&${item}=${params[item]}`
-  })
+    query += `&${item}=${params[item]}`;
+  });
 
   return query;
-}
+};
 
-export const _request = async (method: 'get' | 'post', uri: string, config?: AxiosRequestConfig, query?: string) => {
+export const _request = async (
+  method: 'get' | 'post',
+  uri: string,
+  config?: AxiosRequestConfig,
+  query?: string
+) => {
   const url = `${HOST}/${uri}${query}`;
-  const res = await axios({
-    method,
-    url,
+  const res = await axios[method](url, {
     auth: {
       username: USERNAME,
       password: API_KEY
     },
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     ...config
   });
-  return res;
 
-}
- 
+  return res;
+};
+
 export const get = async (uri: string, config?: AxiosRequestConfig, query?: string) => {
   const res = await _request('get', uri, config, query);
+  return res;
+};
+
+export const post = async (uri: string, config?: AxiosRequestConfig, query?: string) => {
+  const res = await _request('post', uri, config, query);
   return res;
 };

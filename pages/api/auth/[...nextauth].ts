fb57e6d6c31d3f92e18c65dbf41a0ccd06e2a94e
Tom Watson <tomwatson840@gmail.com>
2023-06-02 12:24:40 +0100

    Add /auth api route

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import GithubProvider from 'next-auth/providers/github';
+import NextAuth from 'next-auth';
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code'
+        }
+      }
+    })
+  ]
+};
+
+export default NextAuth(authOptions);

ad7e75fe4d51582d3e86c57f7c718a73794922bc
Tom Watson <tomwatson840@gmail.com>
2023-06-02 17:50:48 +0100

    Get fixture ref from cy.fixture

--- cypress/support/component.tsx
+++ cypress/support/component.tsx
@@ -6,16 +6,17 @@ import { ReactElement } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'styled-components';
 import { mount } from 'cypress/react18';
-import session from '../fixtures/nextAuth/session.json';
 import theme from '@styles/theme';
 
 Cypress.Commands.add(
   'mount',
   ({ component, authenticated = false }: { component: ReactElement; authenticated: boolean }) => {
-    const App = (
-      <SessionProvider session={authenticated ? session : null}>{component}</SessionProvider>
-    );
+    cy.fixture('nextAuth/session').then((session) => {
+      const App = (
+        <SessionProvider session={authenticated ? session : null}>{component}</SessionProvider>
+      );
 
-    return mount(App);
+      return mount(App);
+    });
   }
 );
